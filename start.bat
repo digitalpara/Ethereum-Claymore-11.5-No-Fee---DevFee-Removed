e16dc4a8823a0cc5e061e1457c21d021e843c127
simsek <36822154+digitalpara@users.noreply.github.com>
2018-03-17 12:19:38 +0100

    Add files via upload
    ALL YOUR PAGE ARE BELONG TO US EDITION
    What a jew you have to be to tax on people after earning 5000+ ETH from them mining for you?
    
    Ethereum Claymore 11.5 No Fee - DevFee Removed
    Ethereum Claymore 11.5 No Fee DevFee Removed
    
    Executable is patched so you don't have to do anything, just start mining.
    
    Once devfee mining is triggered it will mine to your address instead of developers. To make sure everything is working properly check the mining .txt logs for any "eth_submitLogin" where your address doesn't appear. If there are any then restart mining again and it should work properly. Otherwise contact me.
    
    Download Claymore 11.5 Ethereum Miner No DevFee Below
    Direct download link: https://mirrorace.com/m/cn7f
    
    FAQ
    How will I know this is working?
    
    Check mining logs for any "devfee" text or "eth_submitLogin" text. If it shows up, make a clean folder and extract ALL files of my Claymore miner there then try again! Does this work for future releases of Claymore ? No, I will manually update future Claymore releases.
    
    I'm getting lower hashrate with your Claymore than original.
    
    Run it for at least 24 hours so that pool statistics even out from downtime where You swapped to Claymore 11.5 No Fee
    
    Claymore 11.5
    Claymore 11.5 Miner
    
    Claymore 11.5 Ethereum Miner No Fee
    
    Claymore 11.5 No Fee
    
    Claymore No Fee 11.5
    
    Claymore Miner Nvidia Ethereum 11.5
    
    Claymore Miner Ethereum 11.5
    
    Claymore Miner 11.5 Ethereum
    
    Claymore No Fee Miner
    
    DevFee Claymore Remove Remove DevFee Claymore

---
+++ start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eu1.ethermine.org:4444 -ewal 0xYour wallet here -eworker rig1 -epsw x -platform 2 -dbg -1 -r 1 -retrydelay 10 -ejobtimeout 1 -minspeed 11.000 -etht 1ms -eres 0 -ethi 16 -etha 0 -mode 0 -mport 0
